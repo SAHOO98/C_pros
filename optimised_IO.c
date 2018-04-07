a5a882065bf4e2d9595d62d8cd3e5905d4c3eee7
SAHOO98 <uzumaki.sahoo@gmail.com>
2018-04-07 19:52:02 +0530

    Corrected Version

--- optimised_IO.c
+++ optimised_IO.c
@@ -9,14 +9,14 @@ void scanf_string(char *c) {
 //optimised input for integers
 void scanf_int(int *n){
 register int c=0;
-register short flag;
+register short flag=0;
 *n = 0;
 c = getchar_unlocked();
 if (c=='-'){
   flag = 1;
+  c = 0;
+  c = getchar_unlocked();
 }
-c = 0;
-c = getchar_unlocked();
 while(c>47 AND c<58){
   *n = (*n<<1)+(*n<<3)+c-48;
   c = getchar_unlocked();
