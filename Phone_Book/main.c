a1f539fee2a01fd3c1c5fe34b5d8b0eafd3daff7
SAHOO98 <uzumaki.sahoo@gmail.com>
2018-04-29 03:17:53 +0530

    Second Edit

--- Phone_Book/main.c
+++ Phone_Book/main.c
@@ -1,11 +1,78 @@
 #include <stdio.h>
+#include <string.h>
+
+#define NEW_PERSON(x) (Person *)malloc(x*sizeof(Person))
+#define EXTEND(ptr,size) (POINT *)realloc(ptr,size*sizeof(POINT))
+
 #define MAX_STRING_LIMIT 100
 #define EMAIL_LENGHT 254
 #define PHONE_NUMBER_LENGHT 13
+#define EXIT_SUCCESS 0
+#define EXIT_PROBLEM0 1 //invalid varialble value
+
+const char *help = "This a Phone book application.\n For:\n Insertion: press 1\nModifications: press 2\nDeletion: press 3\nListing: press 4\nSearching: press 5\nEXIT:press -1\nThank you!!";
+const char *credits = "Applications made by Saptasrhi Sahoo.\nEmail:-uzumaki.sahoo@gmail.com.\nGithub:-https://github.com/SAHOO98";
 
 typedef struct p{
-  char name[MAX_STRING_LIMIT];              //Name of the person
+  char name[MAX_STRING_LIMIT];              //Name of the Person
+  char phone_number[PHONE_NUMBER_LENGHT];   //phone number of the person
   short sex;                                //sex: 1 for male ,and 0 for female
   char email[EMAIL_LENGHT];                 //Email of the person
-  char phone_number[PHONE_NUMBER_LENGHT];   //phone number of the person
 }Person;
+
+Person *head = NULL;
+int COUNT  = 0;
+
+void addrecord(char *name,char *phone_number,char *email,int sex){
+
+  if(head == NULL){
+    head = NEW_PERSON(COUNT+1);
+    strcpy(head->name,name);
+    strcpy(head->phone_number,phone_number);
+    strcpy(head->email,email);
+    head->sex = sex;
+  }else{
+    Person *t
+    t  = head;
+    head = EXTEND(head,COUNT+1);
+    t += COUNT;
+    strcpy(t->name,name);
+    strcpy(t->phone_number,phone_number);
+    strcpy(t->email,email);
+    t->sex = sex;
+
+  }
+  COUNT++;
+}
+int menu(){
+  int choice,sex;
+  char name[MAX_STRING_LIMIT],phone_number[PHONE_NUMBER_LENGHT],email[EMAIL_LENGHT];
+  printf("%s",help);
+  switch (choice){
+    case -1 :
+      return EXIT_SUCCESS;
+    case 1:
+      printf("Enter your name,phone number,Email,sex:-\n" );
+      fgets(name,MAX_STRING_LIMIT,stdin);
+      fgets(phone_number,PHONE_NUMBER_LENGHT,stdin);
+      if(strlen(phone_number)<10)
+       return EXIT_PROBLEM0;
+      fgets(email,EMAIL_LENGHT,stdin);
+      scanf("%d",&sex);
+
+      addrecord(nam,phone_number,email,sex);
+
+
+  }
+
+}
+void start(){
+  printf("Welcome to Elegant phone book.\n", );
+  menu();
+
+
+}
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
--- albus.c
+++ albus.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define NEW_POINT(x) (POINT *)malloc(x*sizeof(POINT))
+#define EXTEND(ptr,size) (POINT *)realloc(ptr,size*sizeof(POINT))
 typedef struct p{
   int x;
   int y;
 }POINT;
-void getPoint(POINT **t) {
-  *t = (POINT *)malloc(sizeof(POINT));
-  (*t)->x = 25;
-  (*t)->y = 36;
+POINT *g;
+void show(POINT *s{
+  printf("x = %d\ty = %d\n",s->x,s->y );
+
+}
+POINT* getPoint() {
+  POINT *t = NEW_POINT(1);
+  t->x = rand()%50;
+  t->y = rand()%50;
+  return t;
+  //memcpy(g,t,sizeof(*t));
 }
 
 int main() {
-  POINT *p1;
-  getPoint(&p1);
+  POINT *temp;
+  srand(time(NULL));
+  g = getPoint();
+  show(g);
+  g  = EXTEND(g,2);
+  show(g);
+  temp = g;
+  temp++;
+  temp = getPoint();
 
-  printf("x = %d\ty = %d\n",p1->x,p1->y );
+  //free(temp);
+  show(g);
+  show(++g);
+
+return 0 ;
 }
